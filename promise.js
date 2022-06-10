cd16b8fb495328315f967cb529ca4a27bf15e06c
Rahmat Riawan <riawanrahmat@gmail.com>
2022-06-10 22:01:00 +0700

    done

--- promise.js
+++ promise.js
@@ -1,8 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (condition) => {
+  let a = await promiseTheaterIXX();
+  let b = await promiseTheaterVGC();
+  let combine = a.concat(b);
+  let count = 0;
+  combine.map((a) => a.hasil === condition && (count += 1));
+  return count;
+};
 module.exports = {
   promiseOutput,
 };
